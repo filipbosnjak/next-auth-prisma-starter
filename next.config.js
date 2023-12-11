399c3c741b83a13aad8da3ccc77ba336f0cdab83
Filip <filipo.bosnjak@gmail.com>
2023-12-11 10:13:54 +0100

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const nextConfig = {
-    images: {
-        domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
-    },
+  swcMinify: true,
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
 };
 
-module.exports = (phase, {defaultConfig}) => {
-    console.log("app startup")
-    return nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  console.log("app startup");
+  return nextConfig;
 };
