9857ad52550e2e5d082bb22364acf7b2041cb1f1
Filip <filipo.bosnjak@gmail.com>
2023-12-11 10:19:26 +0100

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // disabling strict mode since the examples use
+  // useEffect with no dependencies to ensure the function
+  // is only run once and only run on the client.
+  reactStrictMode: false,
   swcMinify: false,
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    dangerouslyAllowSVG: true,
+    domains: [
+      "static.ably.dev",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
   },
 };
 
+module.exports = nextConfig;
+
+/*
 module.exports = (phase, { defaultConfig }) => {
   console.log("app startup");
   return nextConfig;
 };
+*/
